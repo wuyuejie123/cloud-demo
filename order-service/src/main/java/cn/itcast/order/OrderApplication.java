5ef1cab5c02266f23d66cb1786b89a59f46dfb54
peizidiao <2652434301@qq.com>
2022-11-12 21:01:59 +0800

    use nacos 注册中心

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -3,6 +3,9 @@ package cn.itcast.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -13,4 +16,9 @@ public class OrderApplication {
         SpringApplication.run(OrderApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
